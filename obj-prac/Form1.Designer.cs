57747bcbe92c764a1438a2da270f88191291dbfc
YuTanaka <YRK00337@nifty.com>
2016-01-28 21:55:18 +0900

    雛形のプロジェクトを登録

---
+++ obj-prac/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace obj_prac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "面積を求める";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(480, 269);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 322);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "オブジェクト指向演習：四角形と三角形の面積を求める";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ obj-prac/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace obj_prac
+{
+    public partial class Form1 : Form
+    {
+        static Random rand = new Random();
+        /** データの種類*/
+        enum TYPE
+        {
+            SHIKAKU,     // 四角形
+            SANKAKU     // 三角形
+        };
+        /** 種類。処理を分けるのに利用する*/
+        List<TYPE> type = new List<TYPE>();
+        /** 底辺*/
+        List<float> teihen = new List<float>();
+        /** 高さ*/
+        List<float> takasa = new List<float>();
+
+        /** 初期化*/
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 初期化
+            for (int i = 0; i < 5; i++)
+            {
+                // 四角形の作成
+                instantiateShikaku();
+                // 三角形の作成
+                instantiateSankaku();
+            }
+        }
+
+        /** 四角形を作成*/
+        void instantiateShikaku()
+        {
+            // 種類を四角形にする
+            type.Add(TYPE.SHIKAKU);
+            // 底辺に1～10の乱数を求める
+            teihen.Add(rand.Next(1, 11));
+            // 高さに1～99の乱数を求める
+            takasa.Add(rand.Next(1, 11));
+        }
+
+        /** 三角形を作成*/
+        void instantiateSankaku()
+        {
+            // 種類を三角形にする
+            type.Add(TYPE.SANKAKU);
+            // 底辺に1～10の乱数を求める
+            teihen.Add(rand.Next(1, 11));
+            // 高さに1～10の乱数を求める
+            takasa.Add(rand.Next(1, 11));
+        }
+
+        /** ボタンを押したら種類に応じて面積を求めてTextBox1に表示する*/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < type.Count; i++)
+            {
+                textBox1.AppendText(calc(i) + "\r\n");
+            }
+        }
+
+        /** 指定のインデックスの図形の面積を求める*/
+        string calc(int idx)
+        {
+            switch (type[idx])
+            {
+                // 四角形の面積を求めて返す
+                case TYPE.SHIKAKU:
+                    return "四角形：底辺" + teihen[idx] + " 高さ" + takasa[idx] + "=" + (takasa[idx] * teihen[idx]);
+                // 三角形の面積を求めて返す
+                case TYPE.SANKAKU:
+                    return "三角形：底辺" + teihen[idx] + " 高さ" + takasa[idx] + "=" + (teihen[idx] * takasa[idx] / 2f);
+            }
+            // typeが不一致のとき。不要だが念のため
+            return "";
+        }
+    }
+}
